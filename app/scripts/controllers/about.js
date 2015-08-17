355dc45f10465414024b4389d0ca92c5e9c7c4d9
Stephen Kyriacou <Stephen@Unknown-f0-b4-79-21-ca-7a.home>
2015-08-17 02:13:54 +0100

    after installing bower components

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newmag2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the newmag2App
+ */
+angular.module('newmag2App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newmag2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newmag2App
+ */
+angular.module('newmag2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
